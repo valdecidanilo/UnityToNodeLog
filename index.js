035b980f75d5293301dc1143d02987d900c27893
Valdeci Danilo <valdecidanilo@live.com>
2024-03-15 11:04:18 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/receive-log', (req, res) => {
+    const logData = req.body;
+    console.log(`Log: ${logData.message} timestamp: ${logData.timestamp}`);
+    res.send('Log received');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
